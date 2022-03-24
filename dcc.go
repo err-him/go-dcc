7c4ee9586ab82bdd984ba71036d1315b9c8f9660
err-him <himanshu.gupta@sendinblue.com>
2022-03-24 11:37:42 +0530

    makefile changes

--- dcc.go
+++ dcc.go
@@ -1,9 +1,10 @@
 // Package dcc Package provides dcc common code.
 package dcc
 
-// #cgo CFLAGS: -g -Wno-unused-variable
-// #cgo CFLAGS: -I.
-// #cgo LDFLAGS: -L. -lm -lresolv -ldccmj -ldl
+// #cgo CFLAGS: -I./dccsib/include
+// #cgo linux LDFLAGS: -ldl -Wl,--unresolved-symbols=ignore-in-object-files
+// #cgo darwin LDFLAGS: -ldl -Wl,-undefined,dynamic_lookup
+// #cgo LDFLAGS: -L. -lm -lresolv -ldccmj
 // #include <stdlib.h>
 // #include <stdio.h>
 // #include "dccmj.h"
